79820da8c42b489bd659ed431e434faf20c8116b
Thimachal <thimachal@gmail.com>
2023-09-10 18:32:46 -0300

    controller

--- src/main/java/com/thima/AulaSpringThima/resources/CategoryResource.java
+++ src/main/java/com/thima/AulaSpringThima/resources/CategoryResource.java
@@ -1,4 +1,10 @@
 package com.thima.AulaSpringThima.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
 public class CategoryResource {
+
 }
