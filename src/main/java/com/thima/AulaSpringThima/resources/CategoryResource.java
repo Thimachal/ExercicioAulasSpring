5bbfd3d853bb43d79cd4b6daee415033adc8d193
Thimachal <thimachal@gmail.com>
2023-09-10 19:38:47 -0300

    find

--- src/main/java/com/thima/AulaSpringThima/resources/CategoryResource.java
+++ src/main/java/com/thima/AulaSpringThima/resources/CategoryResource.java
@@ -5,11 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
     public ResponseEntity<Category> findAll(){
-
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L,"Books"));
         return null;
     }
 }
