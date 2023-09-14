4e8fa0f978864d693c88eb16c014df44e0204aae
Thimachal <thimachal@gmail.com>
2023-09-13 21:40:13 -0300

    h2 database h2

--- src/main/java/com/thima/AulaSpringThima/services/CategoryService.java
+++ src/main/java/com/thima/AulaSpringThima/services/CategoryService.java
@@ -2,14 +2,16 @@ package com.thima.AulaSpringThima.services;
 
 import com.thima.AulaSpringThima.entities.Category;
 import com.thima.AulaSpringThima.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CategoryService {
+    @Autowired
     private CategoryRepository repository;
     public List<Category> findAll(){
-
+        return repository.findAll();
     }
 }
