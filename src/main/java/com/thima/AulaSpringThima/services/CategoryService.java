cedc1193e801325e1ece393601c86656da5a9a63
Thimachal <thimachal@gmail.com>
2023-09-13 21:09:26 -0300

    repos

--- src/main/java/com/thima/AulaSpringThima/services/CategoryService.java
+++ src/main/java/com/thima/AulaSpringThima/services/CategoryService.java
@@ -1,4 +1,15 @@
 package com.thima.AulaSpringThima.services;
 
+import com.thima.AulaSpringThima.entities.Category;
+import com.thima.AulaSpringThima.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+    private CategoryRepository repository;
+    public List<Category> findAll(){
+
+    }
 }
