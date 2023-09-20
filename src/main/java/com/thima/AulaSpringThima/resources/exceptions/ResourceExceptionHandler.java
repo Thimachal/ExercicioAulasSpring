745d2e2b47814a2db41d982edd1fc94b24f54a44
Thimachal <thimachal@gmail.com>
2023-09-20 16:37:08 -0300

    error

--- src/main/java/com/thima/AulaSpringThima/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/thima/AulaSpringThima/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,25 @@
 package com.thima.AulaSpringThima.resources.exceptions;
 
-public class ResourceExceptionHandler {
+import com.thima.AulaSpringThima.services.exceptions.EntitynotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(EntitynotFoundException.class)
+    public  ResponseEntity<StandardError> entityNotFound(EntitynotFoundException e, HttpServletRequest request){
+    StandardError err = new StandardError();
+    err.setTimestamp(Instant.now());
+    err.setStatus(HttpStatus.NOT_FOUND.value());
+    err.setError("Resource not found");
+    err.setMessage(e.getMessage());
+    err.setPath(request.getRequestURI());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
 }
